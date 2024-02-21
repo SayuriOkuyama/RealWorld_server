ad647f8556ffb665031dd1b24f77b1985d6b6a83
SayuriOkuyama <mosyaneko77@gmail.com>
2024-02-21 18:41:05 +0900

    cors.php 修正

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
   'allowed_methods' => ['*'],
 
-  'allowed_origins' => [env('https://apprentice-client.my-raga-bhakti.com', 'http://localhost:3000')],
+  'allowed_origins' => [env('FRONTEND_URL'), 'http://localhost:3000'],
 
   'allowed_origins_patterns' => [],
 
